dcc00093bfb5b8d1c8460c35fce57cbc8e3e963a
kieranmcm07 <kieransworkstuff@gmail.com>
2025-05-03 16:43:58 +0100

    Initial commit of Currency Converter program

---
+++ Converter.py
@@ -0,0 +1,4 @@
+# Currency Converter by Kieran
+
+# This program converts a given amount of money from one currency to another using exchange rates.
+# The user can choose the currencies and the amount to convert.
