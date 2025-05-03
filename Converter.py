0415f48029ca8066df90d241a01241ed04f1733a
kieranmcm07 <kieransworkstuff@gmail.com>
2025-05-03 17:03:32 +0100

    Add currency converter functionality and menu display

--- Converter.py
+++ Converter.py
@@ -2,3 +2,15 @@
 
 # This program converts a given amount of money from one currency to another using exchange rates.
 # The user can choose the currencies and the amount to convert.
+
+import asciimenu
+
+def menu():
+    
+    print(asciimenu.heading)
+    print("\n\nWelcome to the Currency Converter!")
+    print("Please select the currencies you want to convert from and to.")
+    print("You can choose from the following currencies:")
+
+
+menu()
